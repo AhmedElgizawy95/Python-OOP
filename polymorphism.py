ecd54acfb2c5e1d1cf4040182abcdfac2cdfdd2a
ahmed <elgizawy.ahmed.21@gmail.com>
2026-04-25 22:32:07 +0300

    feat: implemented new class Animal , Dog , Cat and their sounds

--- polymorphism.py
+++ polymorphism.py
@@ -1,1 +1,23 @@
+class Animal:
+    def producesound(self):
+        pass
 
+class Dog(Animal):
+    def producesound(self):
+        return "Woof!"
+    
+
+
+class Cat(Animal):
+    def producesound(self):
+        return "Meow!"
+
+
+
+
+if __name__ == "__main__":
+    dog = Dog()
+    cat = Cat()
+
+    print(f"Dog says: {dog.producesound()}")
+    print(f"Cat says: {cat.producesound()}")
